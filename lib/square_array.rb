4c22aa2122037929f5fe1d97ee875a1dd0a6342a
Milan Winter <milanwinter8@gmail.com>
2020-09-23 20:03:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,8 @@ def square_array(array)
   new_array = []
   counter = 0
   while counter < array.length
-    new_array.push(array[counter]*array[counter])
+    new_array.push(array[counter] * array[counter])
     counter += 1
   end
+  new_array
 end
